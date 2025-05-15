0feca65098bca9d4ff09f352eee316cda4789165
Alvin Yap <leaf.alvin@gmail.com>
2025-05-15 21:35:57 +0800

    fix(mock): fix tylo and style in MockChainlinkOracle

--- src/mocks/MockChainlinkOracle.sol
+++ src/mocks/MockChainlinkOracle.sol
@@ -1,24 +1,24 @@
-""// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract MockChainlinkOracle {
-    int256 private _latestAnswer;
-    string public description;
+    int256 private latestAnswer_;
+    string public description_;
 
     event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
 
     constructor(int256 initialAnswer, string memory _description) {
-        _latestAnswer = initialAnswer;
-        description = _description;
+        latestAnswer_ = initialAnswer;
+        description_ = _description;
         emit AnswerUpdated(initialAnswer, 0, block.timestamp);
     }
 
     function latestAnswer() external view returns (int256) {
-        return _latestAnswer;
+        return latestAnswer_;
     }
 
     function updateAnswer(int256 newAnswer) external {
-        _latestAnswer = newAnswer;
+        latestAnswer_ = newAnswer;
         emit AnswerUpdated(newAnswer, 0, block.timestamp);
     }
 
@@ -41,11 +41,10 @@ contract MockChainlinkOracle {
             uint80 answeredInRound
         )
     {
-        return (0, _latestAnswer, block.timestamp, block.timestamp, 0);
+        return (0, latestAnswer_, block.timestamp, block.timestamp, 0);
     }
 
     function description() external view returns (string memory) {
-        return description;
+        return description_;
     }
 }
-""
