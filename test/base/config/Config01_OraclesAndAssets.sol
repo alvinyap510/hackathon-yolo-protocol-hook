7b93125203c897d6c88732961247a3b8aef53bf6
Alvin Yap <leaf.alvin@gmail.com>
2025-05-16 00:33:32 +0800

    refactor: rename variable name in config to avoid collision

--- test/base/config/Config01_OraclesAndAssets.sol
+++ test/base/config/Config01_OraclesAndAssets.sol
@@ -2,67 +2,67 @@
 pragma solidity ^0.8.0;
 
 contract Config {
-    struct Asset {
+    struct AssetConfig {
         string name;
         string symbol;
         uint256 initialSupply;
     }
 
-    struct Oracle {
+    struct OracleConfig {
         string description;
         int256 initialPrice;
     }
 
-    Asset[] internal assets;
-    Oracle[] internal oracles;
+    AssetConfig[] internal assetsConfig;
+    OracleConfig[] internal oraclesConfig;
 
     constructor() {
         // Initialize Assets
-        assets.push(Asset("Mock DAI", "DAI", 100_000_000 * 1e18));
-        assets.push(Asset("Mock USDC", "USDC", 100_000_000 * 1e6));
-        assets.push(Asset("Mock USDT", "USDT", 100_000_000 * 1e6));
-        assets.push(Asset("Mock USDe", "USDe", 100_000_000 * 1e18));
-        assets.push(Asset("Mock WBTC", "WBTC", 100_000_000 * 1e8));
-        assets.push(Asset("Mock PT-sUSDe-31JUL2025", "PT-sUSDe-31JUL2025", 10_000_000 * 1e18));
-        assets.push(Asset("Mock wstETH", "wstETH", 10_000_000 * 1e18));
+        assetsConfig.push(AssetConfig("Mock DAI", "DAI", 100_000_000 * 1e18));
+        assetsConfig.push(AssetConfig("Mock USDC", "USDC", 100_000_000 * 1e6));
+        assetsConfig.push(AssetConfig("Mock USDT", "USDT", 100_000_000 * 1e6));
+        assetsConfig.push(AssetConfig("Mock USDe", "USDe", 100_000_000 * 1e18));
+        assetsConfig.push(AssetConfig("Mock WBTC", "WBTC", 100_000_000 * 1e8));
+        assetsConfig.push(AssetConfig("Mock PT-sUSDe-31JUL2025", "PT-sUSDe-31JUL2025", 10_000_000 * 1e18));
+        assetsConfig.push(AssetConfig("Mock wstETH", "wstETH", 10_000_000 * 1e18));
 
 
         // Initialize Oracles
-        oracles.push(Oracle("DAI / USD", 1 * 1e8));
-        oracles.push(Oracle("USDC / USD", 1 * 1e8));
-        oracles.push(Oracle("USDT / USD", 1 * 1e8));
-        oracles.push(Oracle("USDe / USD", 1 * 1e8));
-        oracles.push(Oracle("BTC / USD", 104_000 * 1e8));
-        oracles.push(Oracle("PT-sUSDe-31JUL2025 / USD", 2_600 * 1e8));
-        oracles.push(Oracle("wstETH / USD", 3_061_441_798_33));
-        oracles.push(Oracle("ETH / USD", 2_600 * 1e8));
+        oraclesConfig.push(OracleConfig("DAI / USD", 1 * 1e8));
+        oraclesConfig.push(OracleConfig("USDC / USD", 1 * 1e8));
+        oraclesConfig.push(OracleConfig("USDT / USD", 1 * 1e8));
+        oraclesConfig.push(OracleConfig("USDe / USD", 1 * 1e8));
+        oraclesConfig.push(OracleConfig("BTC / USD", 104_000 * 1e8));
+        oraclesConfig.push(OracleConfig("PT-sUSDe-31JUL2025 / USD", 2_600 * 1e8));
+        oraclesConfig.push(OracleConfig("wstETH / USD", 3_061_441_798_33));
+        oraclesConfig.push(OracleConfig("ETH / USD", 2_600 * 1e8));
     }
 
     function getAssetsLength() public view returns (uint256) {
-        return assets.length;
+        return assetsConfig.length;
     }
 
     function getAssetName(uint256 index) public view returns (string memory) {
-        return assets[index].name;
+        return assetsConfig[index].name;
     }
 
     function getAssetSymbol(uint256 index) public view returns (string memory) {
-        return assets[index].symbol;
+        return assetsConfig[index].symbol;
     }
 
     function getAssetInitialSupply(uint256 index) public view returns (uint256) {
-        return assets[index].initialSupply;
+        return assetsConfig[index].initialSupply;
     }
 
     function getOraclesLength() public view returns (uint256) {
-        return oracles.length;
+        return oraclesConfig.length;
     }
 
     function getOracleDescription(uint256 index) public view returns (string memory) {
-        return oracles[index].description;
+        return oraclesConfig[index].description;
     }
 
     function getOracleInitialPrice(uint256 index) public view returns (int256) {
-        return oracles[index].initialPrice;
+        return oraclesConfig[index].initialPrice;
     }
 }
